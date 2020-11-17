fcbe48152ee8a4eff2cb3203a661e45fe9fea1ea
Vatsal Chauhan <chauhanvatsal12@gmail.com>
2020-11-17 16:47:55 -0500

    updated index.js file.

--- myfirstreact/src/index.js
+++ myfirstreact/src/index.js
@@ -4,12 +4,34 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+class Header extends React.Component {
+  render() {
+    return (<p>This is the Header component</p>)
+  }
+}
+class Footer extends React.Component {
+  render() {
+    return (<p>This is the Footer component</p>)
+  }
+}
+class Page extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <p>Hello World !</p>
+        <Footer />
+      </div>
+    )
+  }
+}
+ReactDOM.render(<Page />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
